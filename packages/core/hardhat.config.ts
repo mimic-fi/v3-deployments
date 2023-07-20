b61850940a881c1cff0dfada3ed52afcbcbc7566
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2023-07-20 09:33:40 +0200

    core: setup core deployments package

---
+++ packages/core/hardhat.config.ts
@@ -0,0 +1,11 @@
+import '@mimic-fi/v3-deployments-lib/dist/src/hardhat/deploy'
+import '@nomiclabs/hardhat-ethers'
+import '@nomiclabs/hardhat-waffle'
+import 'hardhat-local-networks-config-plugin'
+
+import { homedir } from 'os'
+import path from 'path'
+
+export default {
+  localNetworksConfig: path.join(homedir(), '/.hardhat/networks.mimic-v3.json'),
+}
