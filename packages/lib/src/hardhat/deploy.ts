ec32d429ac2efbd68595f31609712bd93a951a29
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2023-08-16 10:50:15 +0200

    lib: rename verbose flag to debug

--- packages/lib/src/hardhat/deploy.ts
+++ packages/lib/src/hardhat/deploy.ts
@@ -5,13 +5,14 @@ import { Logger } from '../logger'
 import { Script } from '../script'
 import Verifier from '../verifier'
 
-type DeployArgs = { id: string; key?: string; verbose?: boolean }
+type DeployArgs = { id: string; key?: string; debug?: boolean }
 
 task('deploy', 'Run deployment task')
   .addParam('id', 'Deployment task ID')
+  .addFlag('debug', 'Debug logs enabled')
   .addOptionalParam('key', 'Etherscan API key to verify contracts')
   .setAction(async (args: DeployArgs, hre: HardhatRuntimeEnvironment) => {
-    Logger.setDefaults(false, args.verbose || false)
+    Logger.setDefaults(false, args.debug || false)
     const verifier = args.key ? new Verifier(hre.network, args.key) : undefined
     const script = Script.fromHRE(args.id, hre, verifier)
     await script.run()
--- packages/lib/src/hardhat/verify.ts
+++ packages/lib/src/hardhat/verify.ts
@@ -5,14 +5,15 @@ import { Logger } from '../logger'
 import { Script } from '../script'
 import Verifier from '../verifier'
 
-type VerifyArgs = { id: string; key: string; contract?: string; verbose?: boolean }
+type VerifyArgs = { id: string; key: string; contract?: string; debug?: boolean }
 
 task('verify', 'Run deployment task')
   .addParam('id', 'Deployment task ID')
+  .addFlag('debug', 'Debug logs enabled')
   .addOptionalParam('contract', 'Deployed contract to verify')
   .addParam('key', 'Etherscan API key to verify contracts')
   .setAction(async (args: VerifyArgs, hre: HardhatRuntimeEnvironment) => {
-    Logger.setDefaults(false, args.verbose || false)
+    Logger.setDefaults(false, args.debug || false)
     const verifier = args.key ? new Verifier(hre.network, args.key) : undefined
     const script = Script.fromHRE(args.id, hre, verifier)
     await script.verify(args.contract)
