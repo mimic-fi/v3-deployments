d25c08f31f007246c6e0645dea02378390552cde
DMF <uu@dmfs-mbp.lan>
2023-08-30 10:34:25 -0300

    update grt source and allocationexchange addresses for testing purposes

---
+++ packages/users/the-graph/arbitrum-exchange-allocator/scripts/2023083000-update-for-test-collector-relayer-funder/input.arbitrum.ts
@@ -0,0 +1,38 @@
+import { dependency, DEPLOYER, EnvironmentUpdate, USERS_ADMIN } from '@mimic-fi/v3-deployments-lib'
+
+const update: EnvironmentUpdate = {
+  deployer: dependency('core/deployer/v1.0.0'),
+  namespace: 'the-graph',
+  steps: [
+    {
+      from: USERS_ADMIN,
+      authorizer: dependency('2023071500-environment-deploy', 'authorizer'),
+      changes: [
+        {
+          where: dependency('2023071500-environment-deploy', 'collector-relayer-funder'),
+          grants: [{ who: DEPLOYER.address, what: 'setTokensSource', params: [] }],
+          revokes: [],
+        },
+      ],
+    },
+    {
+      from: DEPLOYER,
+      target: dependency('2023071500-environment-deploy', 'collector-relayer-funder'),
+      method: 'setTokensSource',
+      args: ['0x095a77991EB4dC3727B6f8F95A08321cb2859C86'],
+    },
+    {
+      from: USERS_ADMIN,
+      authorizer: dependency('2023071500-environment-deploy', 'authorizer'),
+      changes: [
+        {
+          where: dependency('2023071500-environment-deploy', 'collector-relayer-funder'),
+          revokes: [{ who: DEPLOYER.address, what: 'setTokensSource' }],
+          grants: [],
+        },
+      ],
+    },
+  ],
+}
+
+export default update
