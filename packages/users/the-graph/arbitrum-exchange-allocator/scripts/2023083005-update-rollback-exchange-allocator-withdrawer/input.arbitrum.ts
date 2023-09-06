fcbed7f4a4af83b960083a6acb95e17ac2d3aeea
Ramiro Gonzalez <ramiro@xivis.com>
2023-09-06 19:20:43 +0200

    The Graph: Rollback test environment changes (#19)
    Co-authored-by: DMF <uu@dmfs-mbp.lan>

---
+++ packages/users/the-graph/arbitrum-exchange-allocator/scripts/2023083005-update-rollback-exchange-allocator-withdrawer/input.arbitrum.ts
@@ -0,0 +1,40 @@
+import { dependency, DEPLOYER, EnvironmentUpdate, USERS_ADMIN } from '@mimic-fi/v3-deployments-lib'
+
+const THE_GRAPH_ALLOCATION_EXCHANGE = '0x993F00C98D1678371a7b261Ed0E0D4b6F42d9aEE'
+
+const update: EnvironmentUpdate = {
+  deployer: dependency('core/deployer/v1.0.0'),
+  namespace: 'the-graph',
+  steps: [
+    {
+      from: USERS_ADMIN,
+      authorizer: dependency('2023071500-environment-deploy', 'authorizer'),
+      changes: [
+        {
+          where: dependency('2023071500-environment-deploy', 'exchange-allocator-withdrawer'),
+          grants: [{ who: DEPLOYER.address, what: 'setRecipient', params: [] }],
+          revokes: [],
+        },
+      ],
+    },
+    {
+      from: DEPLOYER,
+      target: dependency('2023071500-environment-deploy', 'exchange-allocator-withdrawer'),
+      method: 'setRecipient',
+      args: [THE_GRAPH_ALLOCATION_EXCHANGE],
+    },
+    {
+      from: USERS_ADMIN,
+      authorizer: dependency('2023071500-environment-deploy', 'authorizer'),
+      changes: [
+        {
+          where: dependency('2023071500-environment-deploy', 'exchange-allocator-withdrawer'),
+          revokes: [{ who: DEPLOYER.address, what: 'setRecipient' }],
+          grants: [],
+        },
+      ],
+    },
+  ],
+}
+
+export default update
