9a9d172dcfaecd355ee54b7df1286fb670bd1a99
DMF <uu@dmfs-mbp.lan>
2023-09-30 20:42:28 -0300

    1inch: create deploy script

---
+++ packages/users/1inch/airdropper/scripts/2023092700-environment-deploy/input.arbitrum.ts
@@ -0,0 +1,130 @@
+import { OP } from '@mimic-fi/v3-authorizer'
+import {
+  balanceConnectorId,
+  dependency,
+  DEPLOYER,
+  EnvironmentDeployment,
+  MIMIC_V2_BOT,
+  PROTOCOL_ADMIN,
+  USERS_ADMIN,
+} from '@mimic-fi/v3-deployments-lib'
+import { chainlink, fp, NATIVE_TOKEN_ADDRESS, tokens } from '@mimic-fi/v3-helpers'
+
+/* eslint-disable no-secrets/no-secrets */
+
+const TOKEN = 'TBD'
+const ONE_INCH_OWNER = 'TBD'
+const SIGNER = 'TBD'
+const INITIAL_SIGNED_WITHDRAWALS_URL = 'TBD'
+
+const deployment: EnvironmentDeployment = {
+  deployer: dependency('core/deployer/v1.0.0'),
+  namespace: 'the-graph',
+  authorizer: {
+    from: DEPLOYER,
+    name: 'authorizer',
+    version: dependency('core/authorizer/v1.0.0'),
+    owners: [ONE_INCH_OWNER, USERS_ADMIN.safe],
+  },
+  priceOracle: {
+    from: DEPLOYER,
+    name: 'price-oracle',
+    version: dependency('core/price-oracle/v1.0.0'),
+    authorizer: dependency('authorizer'),
+    signer: MIMIC_V2_BOT.address,
+    pivot: chainlink.denominations.USD,
+    feeds: [],
+  },
+  smartVault: {
+    from: DEPLOYER,
+    name: 'smart-vault',
+    version: dependency('core/smart-vault/v1.0.0'),
+    authorizer: dependency('authorizer'),
+    priceOracle: dependency('price-oracle'),
+  },
+  tasks: [
+    {
+      from: DEPLOYER,
+      name: 'depositor',
+      version: dependency('core/tasks/primitives/depositor/v1.0.0'),
+      config: {
+        taskConfig: {
+          baseConfig: {
+            smartVault: dependency('smart-vault'),
+            nextBalanceConnectorId: balanceConnectorId('withdrawer'),
+          },
+          gasLimitConfig: {
+            gasPriceLimit: 10e9,
+          },
+          tokenIndexConfig: {
+            acceptanceType: 1,
+            tokens: [TOKEN],
+          },
+        },
+      },
+    },
+    {
+      from: DEPLOYER,
+      name: 'withdrawer',
+      version: 'OffChainSignedWithdrawer',
+      initialize: 'initialize',
+      args: [SIGNER, INITIAL_SIGNED_WITHDRAWALS_URL],
+      config: {
+        taskConfig: {
+          baseConfig: {
+            smartVault: dependency('smart-vault'),
+            previousBalanceConnectorId: balanceConnectorId('withdrawer'),
+          },
+          gasLimitConfig: {
+            gasPriceLimit: 10e9,
+          },
+          tokenIndexConfig: {
+            acceptanceType: 1,
+            tokens: [TOKEN],
+          },
+        },
+      },
+    },
+  ],
+  permissions: {
+    from: USERS_ADMIN,
+    authorizer: dependency('authorizer'),
+    changes: [
+      {
+        where: dependency('smart-vault'),
+        revokes: [],
+        grants: [
+          { who: dependency('depositor'), what: 'collect', params: [] },
+          { who: dependency('depositor'), what: 'updateBalanceConnector', params: [] },
+          { who: dependency('withdrawer'), what: 'withdraw', params: [] },
+          { who: dependency('withdrawer'), what: 'updateBalanceConnector', params: [] },
+        ],
+      },
+      {
+        where: dependency('depositor'),
+        revokes: [],
+        grants: [{ who: dependency('core/relayer/v1.0.0'), what: 'call', params: [] }],
+      },
+      {
+        where: dependency('withdrawer'),
+        revokes: [],
+        grants: [{ who: dependency('core/relayer/v1.0.0'), what: 'call', params: [] }],
+      },
+    ],
+  },
+  feeSettings: {
+    from: PROTOCOL_ADMIN,
+    smartVault: dependency('smart-vault'),
+    feeController: dependency('core/fee-controller/v1.0.0'),
+    maxFeePct: 0,
+    feePct: 0,
+  },
+  relayerSettings: {
+    from: PROTOCOL_ADMIN,
+    smartVault: dependency('smart-vault'),
+    relayer: dependency('core/relayer/v1.0.0'),
+    quota: 0,
+  },
+}
+
+export default deployment
