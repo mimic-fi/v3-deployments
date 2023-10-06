4bbfc0a8654a0674aab36e0535d7ca09d2d9f06d
Daniel Fernandez <fernandez.daniel@gmail.com>
2023-10-06 23:14:56 +0200

    1inch: Update fee collector pct for tw airdropper (#26)
    Co-authored-by: Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>

---
+++ packages/users/1inch/tw-airdropper/scripts/2023100600-update-fee/input.arbitrum.ts
@@ -0,0 +1,16 @@
+import { dependency, EnvironmentUpdate, PROTOCOL_ADMIN } from '@mimic-fi/v3-deployments-lib'
+
+const update: EnvironmentUpdate = {
+  deployer: dependency('core/deployer/v1.0.0'),
+  namespace: '1inch-tw-airdropper',
+  steps: [
+    {
+      from: PROTOCOL_ADMIN,
+      target: dependency('core/fee-controller/v1.0.0'),
+      method: 'setFeePercentage',
+      args: [dependency('2023092700-environment-deploy', 'smart-vault'), 0],
+    },
+  ],
+}
+
+export default update
