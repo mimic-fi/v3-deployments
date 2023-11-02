bf403e2dac0b7941e568caa077cc69837168a34e
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2023-11-02 16:23:57 -0300

    paraswap: implement custom claimer task

---
+++ packages/users/paraswap/fee-collector/contracts/test/FeeClaimerMock.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import '@openzeppelin/contracts/utils/Address.sol';
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol';
+
+import '@mimic-fi/v3-helpers/contracts/utils/Denominations.sol';
+import '@mimic-fi/v3-helpers/contracts/utils/ERC20Helpers.sol';
+
+import '../interfaces/IFeeClaimer.sol';
+
+contract FeeClaimerMock is IFeeClaimer {
+    bool public fail;
+
+    receive() external payable {
+        // solhint-disable-previous-line no-empty-blocks
+    }
+
+    function mockFail(bool _fail) external {
+        fail = _fail;
+    }
+
+    function augustusSwapper() external pure override returns (address) {
+        return address(0);
+    }
+
+    function getBalance(address token, address) external view override returns (uint256) {
+        return ERC20Helpers.balanceOf(token, address(this));
+    }
+
+    function registerFee(address, address, uint256) external override {
+        // solhint-disable-previous-line no-empty-blocks
+    }
+
+    function withdrawSomeERC20(address token, uint256 amount, address recipient) external override returns (bool) {
+        ERC20Helpers.transfer(token, recipient, amount);
+        return !fail;
+    }
+}
