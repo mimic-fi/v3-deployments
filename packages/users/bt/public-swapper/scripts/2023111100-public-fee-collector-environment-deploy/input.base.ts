40618daeee934ef454cd8e8df540c1eb7ac6bf88
dmf7z <fernandez.daniel@gmail.com>
2023-11-11 21:17:48 -0300

    BT: create draft of env config scripts

---
+++ packages/users/bt/public-swapper/scripts/2023111100-public-fee-collector-environment-deploy/input.base.ts
@@ -0,0 +1,545 @@
+import { OP } from '@mimic-fi/v3-authorizer'
+import {
+  balanceConnectorId,
+  counterfactualDependency,
+  dependency,
+  DEPLOYER,
+  EnvironmentDeployment,
+  MIMIC_V2_BOT,
+  PROTOCOL_ADMIN,
+  USERS_ADMIN,
+} from '@mimic-fi/v3-deployments-lib'
+import { bn, chainlink, fp, NATIVE_TOKEN_ADDRESS, tokens } from '@mimic-fi/v3-helpers'
+
+/* eslint-disable no-secrets/no-secrets */
+
+//Config - Tokens
+const USDC = tokens.base.USDC
+const WRAPPED_NATIVE_TOKEN = tokens.base.WETH
+
+//Config - Addresses
+const OWNER = '0x65226673F3D202E0f897C862590d7e1A992B2048'
+const MAINNET_DEPOSITOR_TASK = '0x85B45B363Ec0023885f86775DdFaf6D879643ED7'
+
+//Config - Threshold
+const USDC_CONVERT_THRESHOLD = bn(100000000) // 100 USDC
+const USDC_BRIDGE_THRESHOLD = bn(1000000000) // 1000 USDC
+
+//Config - Gas
+const STANDARD_GAS_PRICE_LIMIT = 200e9
+const TX_COST_LIMIT_PCT = fp(0.02) // 2%
+const QUOTA = fp(0.000135)
+const MIN_WINDOW_GAS = QUOTA
+const MAX_WINDOW_GAS = QUOTA.mul(10)
+
+//Config - Fee
+const FEE_PCT = fp(0.099) // 9.9%
+
+const deployment: EnvironmentDeployment = {
+  deployer: dependency('core/deployer/v1.0.0'),
+  namespace: 'balancer-fee-collector',
+  authorizer: {
+    from: DEPLOYER,
+    name: 'authorizer',
+    version: dependency('core/authorizer/v1.1.0'),
+    owners: [OWNER, USERS_ADMIN.safe],
+  },
+  priceOracle: {
+    from: DEPLOYER,
+    name: 'price-oracle',
+    version: dependency('core/price-oracle/v1.0.0'),
+    authorizer: dependency('authorizer'),
+    signer: MIMIC_V2_BOT.address,
+    pivot: chainlink.denominations.USD,
+    feeds: [],
+  },
+  smartVault: {
+    from: DEPLOYER,
+    name: 'smart-vault',
+    version: dependency('core/smart-vault/v1.0.0'),
+    authorizer: dependency('authorizer'),
+    priceOracle: dependency('price-oracle'),
+  },
+  tasks: [
+    //Depositor: for manual transfers and testing purposes
+    {
+      from: DEPLOYER,
+      name: 'depositor',
+      version: dependency('core/tasks/primitives/depositor/v2.0.0'),
+      config: {
+        tokensSource: counterfactualDependency('depositor'),
+        taskConfig: {
+          baseConfig: {
+            smartVault: dependency('smart-vault'),
+            nextBalanceConnectorId: balanceConnectorId('swapper-connection'),
+          },
+          gasLimitConfig: {
+            gasPriceLimit: STANDARD_GAS_PRICE_LIMIT,
+          },
+          tokenIndexConfig: {
+            acceptanceType: 0, //Deny list
+            tokens: [],
+          },
+        },
+      },
+    },
+    //Wrapper: wraps native tokens
+    {
+      from: DEPLOYER,
+      name: 'wrapper',
+      version: dependency('core/tasks/primitives/wrapper/v2.0.0'),
+      config: {
+        taskConfig: {
+          baseConfig: {
+            smartVault: dependency('smart-vault'),
+            previousBalanceConnectorId: balanceConnectorId('swapper-connection'),
+            nextBalanceConnectorId: balanceConnectorId('wrapper-handle-over-connection'),
+          },
+          gasLimitConfig: {
+            txCostLimitPct: TX_COST_LIMIT_PCT,
+          },
+          tokenIndexConfig: {
+            acceptanceType: 1, //Allow list
+            tokens: [NATIVE_TOKEN_ADDRESS],
+          },
+          tokenThresholdConfig: {
+            defaultThreshold: {
+              token: USDC,
+              min: USDC_CONVERT_THRESHOLD,
+              max: 0,
+            },
+          },
+        },
+      },
+    },
+    //Handle over: moves exited bpt to be swapped
+    {
+      from: DEPLOYER,
+      name: 'wrapper-handle-over',
+      version: dependency('core/tasks/primitives/handle-over/v2.0.0'),
+      config: {
+        taskConfig: {
+          baseConfig: {
+            smartVault: dependency('smart-vault'),
+            previousBalanceConnectorId: balanceConnectorId('wrapper-handle-over-connection'),
+            nextBalanceConnectorId: balanceConnectorId('swapper-connection'),
+          },
+          tokenIndexConfig: {
+            acceptanceType: 1, //Allow list
+            tokens: [WRAPPED_NATIVE_TOKEN],
+          },
+        },
+      },
+    },
+    //1inch Swapper: swap assets using 1inch dex aggregator
+    {
+      from: DEPLOYER,
+      name: '1inch-swapper',
+      version: dependency('core/tasks/swap/1inch-v5/v2.0.0'),
+      config: {
+        baseSwapConfig: {
+          connector: dependency('core/connectors/1inch-v5/v1.0.0'),
+          tokenOut: USDC,
+          maxSlippage: fp(0.02), //2%
+          customTokensOut: [],
+          customMaxSlippages: [],
+          taskConfig: {
+            baseConfig: {
+              smartVault: dependency('smart-vault'),
+              previousBalanceConnectorId: balanceConnectorId('swapper-connection'),
+              nextBalanceConnectorId: balanceConnectorId('bridger-connection'),
+            },
+            gasLimitConfig: {
+              txCostLimitPct: TX_COST_LIMIT_PCT,
+            },
+            tokenIndexConfig: {
+              acceptanceType: 0,
+              tokens: [USDC],
+            },
+            tokenThresholdConfig: {
+              defaultThreshold: {
+                token: USDC,
+                min: USDC_CONVERT_THRESHOLD,
+                max: 0,
+              },
+            },
+          },
+        },
+      },
+    },
+    //Paraswap Swapper: swap assets using Paraswap dex aggregator
+    {
+      from: DEPLOYER,
+      name: 'paraswap-swapper',
+      version: dependency('core/tasks/swap/paraswap-v5/v2.0.0'),
+      config: {
+        quoteSigner: '0x6278c27cf5534f07fa8f1ab6188a155cb8750ffa',
+        baseSwapConfig: {
+          connector: dependency('core/connectors/paraswap-v5/v1.0.0'),
+          tokenOut: USDC,
+          maxSlippage: fp(0.02), //2%
+          customTokensOut: [],
+          customMaxSlippages: [],
+          taskConfig: {
+            baseConfig: {
+              smartVault: dependency('smart-vault'),
+              previousBalanceConnectorId: balanceConnectorId('swapper-connection'),
+              nextBalanceConnectorId: balanceConnectorId('bridger-connection'),
+            },
+            gasLimitConfig: {
+              txCostLimitPct: TX_COST_LIMIT_PCT,
+            },
+            tokenIndexConfig: {
+              acceptanceType: 0,
+              tokens: [USDC],
+            },
+            tokenThresholdConfig: {
+              defaultThreshold: {
+                token: USDC,
+                min: USDC_CONVERT_THRESHOLD,
+                max: 0,
+              },
+            },
+          },
+        },
+      },
+    },
+    //Handle over: moves USDC to be bridged
+    {
+      from: DEPLOYER,
+      name: 'usdc-handle-over',
+      version: dependency('core/tasks/primitives/handle-over/v2.0.0'),
+      config: {
+        taskConfig: {
+          baseConfig: {
+            smartVault: dependency('smart-vault'),
+            previousBalanceConnectorId: balanceConnectorId('swapper-connection'),
+            nextBalanceConnectorId: balanceConnectorId('bridger-connection'),
+          },
+          tokenIndexConfig: {
+            acceptanceType: 1, //Allow list
+            tokens: [USDC],
+          },
+        },
+      },
+    },
+    //Bridger by threshold: bridges every certain threshold
+    {
+      from: DEPLOYER,
+      name: 'cctp-bridger-by-threshold',
+      version: dependency('core/tasks/bridge/wormhole/v2.0.0'),
+      config: {
+        baseBridgeConfig: {
+          connector: dependency('core/connectors/wormhole/v1.0.0'),
+          recipient: MAINNET_DEPOSITOR_TASK,
+          destinationChain: 1, // mainnet
+          maxSlippage: fp(0.02), //2%
+          maxFee: {
+            token: USDC,
+            amount: fp(0.02), //2%
+          },
+          customDestinationChains: [],
+          customMaxSlippages: [],
+          customMaxFees: [],
+          taskConfig: {
+            baseConfig: {
+              smartVault: dependency('smart-vault'),
+              previousBalanceConnectorId: balanceConnectorId('bridger-connection'),
+            },
+            tokenIndexConfig: {
+              acceptanceType: 1,
+              tokens: [USDC],
+            },
+            tokenThresholdConfig: {
+              defaultThreshold: {
+                token: USDC,
+                min: USDC_BRIDGE_THRESHOLD,
+                max: 0,
+              },
+            },
+          },
+        },
+      },
+    },
+    //Bridger by time: makes sure that by the end of the period, it bridges everything
+    {
+      from: DEPLOYER,
+      name: 'cctp-bridger-by-time',
+      version: dependency('core/tasks/bridge/wormhole/v2.0.0'),
+      config: {
+        baseBridgeConfig: {
+          connector: dependency('core/connectors/wormhole/v1.0.0'),
+          recipient: MAINNET_DEPOSITOR_TASK,
+          destinationChain: 1, // mainnet
+          maxSlippage: fp(0.02), //2%
+          maxFee: {
+            token: USDC,
+            amount: fp(0.02), //2%
+          },
+          customDestinationChains: [],
+          customMaxSlippages: [],
+          customMaxFees: [],
+          taskConfig: {
+            baseConfig: {
+              smartVault: dependency('smart-vault'),
+              previousBalanceConnectorId: balanceConnectorId('bridger-connection'),
+            },
+            tokenIndexConfig: {
+              acceptanceType: 1,
+              tokens: [USDC],
+            },
+            timeLockConfig: {
+              //TODO: CHANGE TO BE LAST DAY OF THE MONTH!!!!!!!!!!!!!!!!!!!!!!!!!!
+              mode: 1, //SECONDS
+              frequency: 14 * 60 * 60 * 24, //14 days
+              allowedAt: 1699524000, //9 Nov
+              window: 2 * 60 * 60 * 24, //2 days
+            },
+          },
+        },
+      },
+    },
+    //Relayer Funder Swapper: swaps assets into native wrapped token to fund the relayer
+    {
+      from: DEPLOYER,
+      name: 'relayer-funder-swapper',
+      version: dependency('core/tasks/swap/1inch-v5/v2.0.0'),
+      config: {
+        baseSwapConfig: {
+          connector: dependency('core/connectors/1inch-v5/v1.0.0'),
+          tokenOut: WRAPPED_NATIVE_TOKEN,
+          maxSlippage: fp(0.02),
+          customTokensOut: [],
+          customMaxSlippages: [],
+          taskConfig: {
+            baseConfig: {
+              smartVault: dependency('smart-vault'),
+              previousBalanceConnectorId: balanceConnectorId('bridger-connection'),
+              nextBalanceConnectorId: balanceConnectorId('relayer-funder-unwrapper'),
+            },
+            gasLimitConfig: {
+              gasPriceLimit: STANDARD_GAS_PRICE_LIMIT,
+            },
+            tokenIndexConfig: {
+              acceptanceType: 1,
+              tokens: [USDC],
+            },
+            tokenThresholdConfig: {
+              defaultThreshold: {
+                token: WRAPPED_NATIVE_TOKEN,
+                min: MIN_WINDOW_GAS,
+                max: MAX_WINDOW_GAS,
+              },
+            },
+          },
+        },
+      },
+    },
+    //Relayer Funder Unwrapper: unwraps wrapped native token
+    {
+      from: DEPLOYER,
+      name: 'relayer-funder-unwrapper',
+      version: dependency('core/tasks/primitives/unwrapper/v2.0.0'),
+      config: {
+        taskConfig: {
+          baseConfig: {
+            smartVault: dependency('smart-vault'),
+            previousBalanceConnectorId: balanceConnectorId('relayer-funder-unwrapper'),
+            nextBalanceConnectorId: balanceConnectorId('relayer-depositor'),
+          },
+          gasLimitConfig: {
+            gasPriceLimit: STANDARD_GAS_PRICE_LIMIT,
+          },
+          tokenIndexConfig: {
+            acceptanceType: 1,
+            tokens: [WRAPPED_NATIVE_TOKEN],
+          },
+        },
+      },
+    },
+    //Relayer Depositor: transfer and funds the relayer
+    {
+      from: DEPLOYER,
+      name: 'relayer-depositor',
+      version: dependency('core/tasks/relayer/depositor/v2.0.0'),
+      args: [dependency('core/relayer/v1.1.0')],
+      config: {
+        baseConfig: {
+          smartVault: dependency('smart-vault'),
+          previousBalanceConnectorId: balanceConnectorId('relayer-depositor'),
+        },
+        gasLimitConfig: {
+          gasPriceLimit: STANDARD_GAS_PRICE_LIMIT,
+        },
+        tokenIndexConfig: {
+          acceptanceType: 1,
+          tokens: [NATIVE_TOKEN_ADDRESS],
+        },
+      },
+    },
+  ],
+  permissions: {
+    from: USERS_ADMIN,
+    authorizer: dependency('authorizer'),
+    changes: [
+      {
+        where: dependency('smart-vault'),
+        revokes: [],
+        grants: [
+          { who: dependency('depositor'), what: 'collect', params: [] },
+          {
+            who: dependency('depositor'),
+            what: 'updateBalanceConnector',
+            params: [],
+          },
+          {
+            who: dependency('wrapper'),
+            what: 'wrap',
+            params: [],
+          },
+          {
+            who: dependency('wrapper'),
+            what: 'updateBalanceConnector',
+            params: [],
+          },
+          {
+            who: dependency('wrapper-handle-over'),
+            what: 'updateBalanceConnector',
+            params: [],
+          },
+          {
+            who: dependency('1inch-swapper'),
+            what: 'execute',
+            params: [],
+          },
+          {
+            who: dependency('1inch-swapper'),
+            what: 'updateBalanceConnector',
+            params: [],
+          },
+          {
+            who: dependency('paraswap-swapper'),
+            what: 'execute',
+            params: [],
+          },
+          {
+            who: dependency('paraswap-swapper'),
+            what: 'updateBalanceConnector',
+            params: [],
+          },
+          {
+            who: dependency('usdc-handle-over'),
+            what: 'updateBalanceConnector',
+            params: [],
+          },
+          {
+            who: dependency('cctp-bridger'),
+            what: 'execute',
+            params: [],
+          },
+          {
+            who: dependency('cctp-bridger'),
+            what: 'updateBalanceConnector',
+            params: [],
+          },
+          {
+            who: dependency('relayer-funder-swapper'),
+            what: 'execute',
+            params: [
+              {
+                op: OP.EQ,
+                value: dependency('core/connectors/1inch-v5/v1.0.0'),
+              },
+            ],
+          },
+          {
+            who: dependency('relayer-funder-swapper'),
+            what: 'updateBalanceConnector',
+            params: [],
+          },
+          {
+            who: dependency('relayer-funder-unwrapper'),
+            what: 'unwrap',
+            params: [],
+          },
+          {
+            who: dependency('relayer-funder-unwrapper'),
+            what: 'updateBalanceConnector',
+            params: [],
+          },
+          { who: dependency('relayer-depositor'), what: 'call', params: [] },
+          {
+            who: dependency('relayer-depositor'),
+            what: 'updateBalanceConnector',
+            params: [],
+          },
+        ],
+      },
+      {
+        where: dependency('depositor'),
+        revokes: [],
+        grants: [{ who: dependency('core/relayer/v1.1.0'), what: 'call', params: [] }],
+      },
+      {
+        where: dependency('wrapper'),
+        revokes: [],
+        grants: [{ who: dependency('core/relayer/v1.1.0'), what: 'call', params: [] }],
+      },
+      {
+        where: dependency('wrapper-handle-over'),
+        revokes: [],
+        grants: [{ who: dependency('core/relayer/v1.1.0'), what: 'call', params: [] }],
+      },
+      {
+        where: dependency('1inch-swapper'),
+        revokes: [],
+        grants: [{ who: dependency('core/relayer/v1.1.0'), what: 'call', params: [] }],
+      },
+      {
+        where: dependency('paraswap-swapper'),
+        revokes: [],
+        grants: [{ who: dependency('core/relayer/v1.1.0'), what: 'call', params: [] }],
+      },
+      {
+        where: dependency('usdc-handle-over'),
+        revokes: [],
+        grants: [{ who: dependency('core/relayer/v1.1.0'), what: 'call', params: [] }],
+      },
+      {
+        where: dependency('cctp-bridger'),
+        revokes: [],
+        grants: [{ who: dependency('core/relayer/v1.1.0'), what: 'call', params: [] }],
+      },
+      {
+        where: dependency('relayer-funder-swapper'),
+        revokes: [],
+        grants: [{ who: dependency('core/relayer/v1.1.0'), what: 'call', params: [] }],
+      },
+      {
+        where: dependency('relayer-funder-unwrapper'),
+        revokes: [],
+        grants: [{ who: dependency('core/relayer/v1.1.0'), what: 'call', params: [] }],
+      },
+      {
+        where: dependency('relayer-depositor'),
+        revokes: [],
+        grants: [{ who: dependency('core/relayer/v1.1.0'), what: 'call', params: [] }],
+      },
+    ],
+  },
+  feeSettings: {
+    from: PROTOCOL_ADMIN,
+    smartVault: dependency('smart-vault'),
+    feeController: dependency('core/fee-controller/v1.0.0'),
+    maxFeePct: fp(0.02), // 2%
+    feePct: FEE_PCT,
+  },
+  relayerSettings: {
+    from: PROTOCOL_ADMIN,
+    smartVault: dependency('smart-vault'),
+    relayer: dependency('core/relayer/v1.1.0'),
+    quota: QUOTA,
+  },
+}
+
+export default deployment
---
+++ packages/users/bt/public-swapper/scripts/2023111101-public-swapper-environment-app1-deploy/input.base.ts
@@ -0,0 +1,120 @@
+import { OP } from '@mimic-fi/v3-authorizer'
+import {
+  dependency,
+  DEPLOYER,
+  EnvironmentDeployment,
+  MIMIC_V2_BOT,
+  PROTOCOL_ADMIN,
+  USERS_ADMIN,
+} from '@mimic-fi/v3-deployments-lib'
+import { chainlink, fp } from '@mimic-fi/v3-helpers'
+
+/* eslint-disable no-secrets/no-secrets */
+
+//Config - Gas
+const QUOTA = 0
+
+//Config - Fee
+const FEE_PCT = fp(0.099) // 9.9%
+
+const deployment: EnvironmentDeployment = {
+  deployer: dependency('core/deployer/v1.0.0'),
+  namespace: 'balancer-fee-collector',
+  authorizer: {
+    from: DEPLOYER,
+    name: 'authorizer',
+    version: dependency('core/authorizer/v1.1.0'),
+    owners: [USERS_ADMIN.safe],
+  },
+  priceOracle: {
+    from: DEPLOYER,
+    name: 'price-oracle',
+    version: dependency('core/price-oracle/v1.0.0'),
+    authorizer: dependency('authorizer'),
+    signer: MIMIC_V2_BOT.address,
+    pivot: chainlink.denominations.USD,
+    feeds: [],
+  },
+  smartVault: {
+    from: DEPLOYER,
+    name: 'smart-vault',
+    version: dependency('core/smart-vault/v1.0.0'),
+    authorizer: dependency('authorizer'),
+    priceOracle: dependency('price-oracle'),
+  },
+  tasks: [
+    //1inch public swapper
+    {
+      from: DEPLOYER,
+      name: '1inch-v5-public-swapper',
+      version: 'OneInchV5PublicSwapper',
+      args: [], //TODO: need a connector????!!!!!!
+      config: {
+        baseConfig: {
+          smartVault: dependency('smart-vault'), //TODO: does not have prev o next!!!!!!!!
+        },
+        tokenIndexConfig: {
+          acceptanceType: 0, //Deny list
+          tokens: [],
+        },
+      },
+    },
+  ],
+  permissions: {
+    from: USERS_ADMIN,
+    authorizer: dependency('authorizer'),
+    changes: [
+      {
+        where: dependency('smart-vault'),
+        revokes: [],
+        grants: [
+          {
+            who: dependency('1inch-v5-public-swapper'),
+            what: 'collect',
+            params: [],
+          },
+          {
+            who: dependency('1inch-v5-public-swapper'),
+            what: 'execute',
+            params: [
+              {
+                op: OP.EQ,
+                value: dependency('core/connectors/1inch-v5/v1.0.0'),
+              },
+            ],
+          },
+          {
+            who: dependency('1inch-v5-public-swapper'),
+            what: 'wrap',
+            params: [],
+          },
+          {
+            who: dependency('1inch-v5-public-swapper'),
+            what: 'withdraw',
+            params: [],
+          },
+        ],
+      },
+      {
+        where: dependency('depositor'),
+        revokes: [],
+        grants: [{ who: ALL, what: 'call', params: [] }], //TODO: how to define everyone
+      },
+    ],
+  },
+  feeSettings: {
+    from: PROTOCOL_ADMIN,
+    smartVault: dependency('smart-vault'),
+    feeController: dependency('core/fee-controller/v1.0.0'), //TODO: how to redirect fee controller
+    maxFeePct: fp(0.02), // 2%
+    feePct: FEE_PCT,
+  },
+  relayerSettings: {
+    from: PROTOCOL_ADMIN,
+    smartVault: dependency('smart-vault'),
+    relayer: dependency('core/relayer/v1.1.0'),
+    quota: QUOTA,
+  },
+}
+
+export default deployment
