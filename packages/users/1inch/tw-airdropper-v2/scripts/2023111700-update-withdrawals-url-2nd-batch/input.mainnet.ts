f758cc0124717403d1e5dfe4a5a442af8163577f
dmf7z <fernandez.daniel@gmail.com>
2023-11-17 15:04:43 -0300

    1inch: update withdrawals url to run second batch

---
+++ packages/users/1inch/tw-airdropper-v2/scripts/2023111700-update-withdrawals-url-2nd-batch/input.mainnet.ts
@@ -0,0 +1,40 @@
+import { dependency, DEPLOYER, EnvironmentUpdate, USERS_ADMIN } from '@mimic-fi/v3-deployments-lib'
+
+const WITHDRAWALS_URL = 'https://www.jsonkeeper.com/b/KQJS'
+
+const update: EnvironmentUpdate = {
+  deployer: dependency('core/deployer/v1.0.0'),
+  namespace: '1inch-tw-airdropper-v2',
+  steps: [
+    {
+      from: USERS_ADMIN,
+      authorizer: dependency('2023101900-environment-deploy', 'authorizer'),
+      changes: [
+        {
+          where: dependency('2023101900-environment-deploy', 'withdrawer'),
+          grants: [{ who: DEPLOYER.address, what: 'setSignedWithdrawalsUrl', params: [] }],
+          revokes: [],
+        },
+      ],
+    },
+    {
+      from: DEPLOYER,
+      target: dependency('2023101900-environment-deploy', 'withdrawer'),
+      method: 'setSignedWithdrawalsUrl',
+      args: [WITHDRAWALS_URL],
+    },
+    {
+      from: USERS_ADMIN,
+      authorizer: dependency('2023101900-environment-deploy', 'authorizer'),
+      changes: [
+        {
+          where: dependency('2023101900-environment-deploy', 'withdrawer'),
+          revokes: [{ who: DEPLOYER.address, what: 'setSignedWithdrawalsUrl' }],
+          grants: [],
+        },
+      ],
+    },
+  ],
+}
+
+export default update
