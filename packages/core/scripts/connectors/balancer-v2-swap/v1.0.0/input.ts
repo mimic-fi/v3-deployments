320366cfb86860a5114c56dc63fdc114b6db09ef
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2023-11-22 11:32:34 -0300

    core: deploy new balancer connectors and tasks to avax

---
+++ packages/core/scripts/connectors/balancer-v2-swap/v1.0.0/input.ts
@@ -0,0 +1,27 @@
+import { dependency, PROTOCOL_ADMIN, RegistryImplementationDeployment } from '@mimic-fi/v3-deployments-lib'
+import { protocols } from '@mimic-fi/v3-helpers'
+
+const shared = {
+  from: PROTOCOL_ADMIN,
+  registry: dependency('core/registry/v1.0.0'),
+  name: 'balancer-v2-swap-connector@v1.0.0',
+  contract: 'BalancerV2SwapConnector',
+  stateless: true,
+}
+
+const mainnet: RegistryImplementationDeployment = {
+  ...shared,
+  args: [protocols.mainnet.BALANCER_V2_VAULT],
+}
+
+const avalanche: RegistryImplementationDeployment = {
+  ...shared,
+  args: [protocols.avalanche.BALANCER_V2_VAULT],
+}
+
+const base: RegistryImplementationDeployment = {
+  ...shared,
+  args: [protocols.base.BALANCER_V2_VAULT],
+}
+
+export default { mainnet, avalanche, base }
---
+++ packages/core/test/connectors/balancer-v2-pool/v1.0.0.fork.ts
@@ -0,0 +1,28 @@
+import { Script } from '@mimic-fi/v3-deployments-lib'
+import { expect } from 'chai'
+import { Contract } from 'ethers'
+import hre from 'hardhat'
+
+const script = Script.forForkedNetwork('connectors/balancer-v2-pool/v1.0.0', hre)
+const test = script.hasInput ? describe : describe.skip
+
+test(script.id, () => {
+  let balancerConnector: Contract, registry: Contract
+
+  before('deploy registry', async () => {
+    const registryScript = Script.forForkedNetwork('registry/v1.0.0', hre)
+    await registryScript.run()
+    registry = await registryScript.deployedInstance('Registry')
+  })
+
+  before('run script', async () => {
+    await script.run()
+    balancerConnector = await script.deployedInstance('BalancerV2PoolConnector')
+  })
+
+  it('registers the balancer v2 pool connector in the registry correctly', async () => {
+    expect(await registry.isRegistered(balancerConnector.address)).to.be.true
+    expect(await registry.isStateless(balancerConnector.address)).to.be.true
+    expect(await registry.isDeprecated(balancerConnector.address)).to.be.false
+  })
+})
