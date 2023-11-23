4b6b755ba76a0e665e23dfecadce74f5e80fbb51
dmf7z <fernandez.daniel@gmail.com>
2023-11-22 22:45:22 -0300

    balancer: execute scripts to process current cycle

--- packages/users/balancer/fee-collector/scripts/2023112204-update-fees/input.base.ts
+++ packages/users/balancer/fee-collector/scripts/2023112204-update-fees/input.base.ts
@@ -1,15 +1,17 @@
 import { dependency, EnvironmentUpdate, PROTOCOL_ADMIN } from '@mimic-fi/v3-deployments-lib'
 import { fp } from '@mimic-fi/v3-helpers'
 
+const FEE_PCT = fp(0.02) // 2%
+
 const update: EnvironmentUpdate = {
   deployer: dependency('core/deployer/v1.0.0'),
   namespace: 'balancer-fee-collector',
   steps: [
     {
       from: PROTOCOL_ADMIN,
-      target: dependency('core/relayer/v1.1.0'),
-      method: 'setSmartVaultMaxQuota',
-      args: [dependency('2023101700-environment-deploy', 'smart-vault'), fp(3)],
+      target: dependency('core/fee-controller/v1.0.0'),
+      method: 'setFeePercentage',
+      args: [dependency('2023101700-environment-deploy', 'smart-vault'), FEE_PCT],
     },
   ],
 }
