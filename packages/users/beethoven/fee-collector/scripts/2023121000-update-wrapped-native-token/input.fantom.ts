02b04b8d7bbf5db3b9a196f042ac18be56c0714a
dmf7z <fernandez.daniel@gmail.com>
2023-12-11 08:59:15 -0300

    beethoven: replace threshold with WFTM

--- packages/users/beethoven/fee-collector/scripts/2023121000-update-wrapped-native-token/input.fantom.ts
+++ packages/users/beethoven/fee-collector/scripts/2023121000-update-wrapped-native-token/input.fantom.ts
@@ -1,8 +1,12 @@
 import { dependency, DEPLOYER, EnvironmentUpdate, USERS_ADMIN } from '@mimic-fi/v3-deployments-lib'
-import { tokens } from '@mimic-fi/v3-helpers'
+import { fp, tokens } from '@mimic-fi/v3-helpers'
 
 /* eslint-disable no-secrets/no-secrets */
 const WRAPPED_NATIVE_TOKEN = tokens.fantom.WFTM
+
+const QUOTA = fp(0.79).mul(10)
+const MIN_WINDOW_GAS = QUOTA
+const MAX_WINDOW_GAS = QUOTA.mul(10)
 
 const update: EnvironmentUpdate = {
   deployer: dependency('core/deployer/v1.0.0'),
@@ -17,6 +21,11 @@ const update: EnvironmentUpdate = {
           grants: [{ who: DEPLOYER.address, what: 'setDefaultTokenOut', params: [] }],
           revokes: [],
         },
+        {
+          where: dependency('2023120300-replace-relayer-funder-swapper', 'relayer-funder-swapper-v2'),
+          grants: [{ who: DEPLOYER.address, what: 'setDefaultTokenThreshold', params: [] }],
+          revokes: [],
+        },
       ],
     },
     {
@@ -26,12 +35,23 @@ const update: EnvironmentUpdate = {
       args: [WRAPPED_NATIVE_TOKEN],
     },
     {
+      from: DEPLOYER,
+      target: dependency('2023120300-replace-relayer-funder-swapper', 'relayer-funder-swapper-v2'),
+      method: 'setDefaultTokenThreshold',
+      args: [WRAPPED_NATIVE_TOKEN, MIN_WINDOW_GAS, MAX_WINDOW_GAS],
+    },
+    {
       from: USERS_ADMIN,
       authorizer: dependency('2023111100-environment-deploy', 'authorizer'),
       changes: [
         {
           where: dependency('2023120300-replace-relayer-funder-swapper', 'relayer-funder-swapper-v2'),
           revokes: [{ who: DEPLOYER.address, what: 'setDefaultTokenOut' }],
+          grants: [],
+        },
+        {
+          where: dependency('2023120300-replace-relayer-funder-swapper', 'relayer-funder-swapper-v2'),
+          revokes: [{ who: DEPLOYER.address, what: 'setDefaultTokenThreshold' }],
           grants: [],
         },
       ],
