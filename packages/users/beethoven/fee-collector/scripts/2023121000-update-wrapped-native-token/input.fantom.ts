fe66be4355808b95cd737f85bb992fa203f2d375
dmf7z <fernandez.daniel@gmail.com>
2023-12-10 21:04:27 -0300

    beethoven: replace WETH with WFTM

---
+++ packages/users/beethoven/fee-collector/scripts/2023121000-update-wrapped-native-token/input.fantom.ts
@@ -0,0 +1,42 @@
+import { dependency, DEPLOYER, EnvironmentUpdate, USERS_ADMIN } from '@mimic-fi/v3-deployments-lib'
+import { tokens } from '@mimic-fi/v3-helpers'
+
+/* eslint-disable no-secrets/no-secrets */
+const WRAPPED_NATIVE_TOKEN = tokens.fantom.WFTM
+
+const update: EnvironmentUpdate = {
+  deployer: dependency('core/deployer/v1.0.0'),
+  namespace: 'beethoven-fee-collector-v3.0',
+  steps: [
+    {
+      from: USERS_ADMIN,
+      authorizer: dependency('2023111100-environment-deploy', 'authorizer'),
+      changes: [
+        {
+          where: dependency('2023120300-replace-relayer-funder-swapper', 'relayer-funder-swapper-v2'),
+          grants: [{ who: DEPLOYER.address, what: 'setDefaultTokenOut', params: [] }],
+          revokes: [],
+        },
+      ],
+    },
+    {
+      from: DEPLOYER,
+      target: dependency('2023120300-replace-relayer-funder-swapper', 'relayer-funder-swapper-v2'),
+      method: 'setDefaultTokenOut',
+      args: [WRAPPED_NATIVE_TOKEN],
+    },
+    {
+      from: USERS_ADMIN,
+      authorizer: dependency('2023111100-environment-deploy', 'authorizer'),
+      changes: [
+        {
+          where: dependency('2023120300-replace-relayer-funder-swapper', 'relayer-funder-swapper-v2'),
+          revokes: [{ who: DEPLOYER.address, what: 'setDefaultTokenOut' }],
+          grants: [],
+        },
+      ],
+    },
+  ],
+}
+
+export default update
