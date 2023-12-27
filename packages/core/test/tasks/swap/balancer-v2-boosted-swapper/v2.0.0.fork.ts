80f3544cd7147b977607450bb1b4e95b11fc6f8d
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2023-12-27 17:36:16 -0300

    Core: Deploy zkEVM tasks v0.3.1 (#63)

---
+++ packages/core/test/tasks/swap/balancer-v2-boosted-swapper/v2.0.0.fork.ts
@@ -0,0 +1,28 @@
+import { Script } from '@mimic-fi/v3-deployments-lib'
+import { expect } from 'chai'
+import { Contract } from 'ethers'
+import hre from 'hardhat'
+
+const script = Script.forForkedNetwork('tasks/swap/balancer-v2-boosted-swapper/v2.0.0', hre)
+const test = script.hasInput ? describe : describe.skip
+
+test(script.id, () => {
+  let balancerV2BoostedSwapper: Contract, registry: Contract
+
+  before('deploy registry', async () => {
+    const registryScript = Script.forForkedNetwork('registry/v1.0.0', hre)
+    await registryScript.run()
+    registry = await registryScript.deployedInstance('Registry')
+  })
+
+  before('run script', async () => {
+    await script.run()
+    balancerV2BoostedSwapper = await script.deployedInstance('BalancerV2BoostedSwapper')
+  })
+
+  it('registers the balancer v2 boosted swapper in the registry correctly', async () => {
+    expect(await registry.isRegistered(balancerV2BoostedSwapper.address)).to.be.true
+    expect(await registry.isStateless(balancerV2BoostedSwapper.address)).to.be.false
+    expect(await registry.isDeprecated(balancerV2BoostedSwapper.address)).to.be.false
+  })
+})
