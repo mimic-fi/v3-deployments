fdaaddc3e8b491406fa9b783988079c90728e2e5
dmf7z <fernandez.daniel@gmail.com>
2023-12-28 15:22:25 -0300

    exodus: deploy env for non 1inch fees

---
+++ packages/users/exodus/fee-collector/scripts/2023122801-update-fee/input.mainnet.ts
@@ -0,0 +1,24 @@
+import {
+  balanceConnectorId,
+  dependency,
+  DEPLOYER,
+  EnvironmentUpdate,
+  PROTOCOL_ADMIN,
+} from '@mimic-fi/v3-deployments-lib'
+import { fp } from '@mimic-fi/v3-helpers'
+
+const update: EnvironmentUpdate = {
+  deployer: dependency('core/deployer/v1.0.0'),
+  namespace: 'exodus-fee-collector',
+  steps: [
+    {
+      from: PROTOCOL_ADMIN,
+      smartVault: dependency('2023121800-environment-deploy', 'smart-vault'),
+      feeController: dependency('core/fee-controller/v1.0.0'),
+      maxFeePct: fp(0.02), // 2%
+      feePct: 0,
+    },
+  ],
+}
+
+export default update
