d9a7accecc70fd85fdbc546cdddb944affd09503
Daniel Fernandez <fernandez.daniel@gmail.com>
2024-01-02 16:23:03 -0300

    Rainbow: Deploy environment (#57)
    Co-authored-by: PatricioHenderson <72527088+PatricioHenderson@users.noreply.github.com>
    Co-authored-by: Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>

---
+++ packages/users/rainbow/fee-collector/contracts/test/FeeCollectorMock.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import '@mimic-fi/v3-helpers/contracts/utils/ERC20Helpers.sol';
+
+import '../interfaces/IFeeCollector.sol';
+
+contract FeeCollectorMock is IFeeCollector {
+    bool public fail;
+
+    receive() external payable {
+        // solhint-disable-previous-line no-empty-blocks
+    }
+
+    function collectETHFees(uint256 amount) external override {
+        (bool success, ) = address(msg.sender).call{ value: amount }('');
+    }
+
+    function collectTokenFees(address token, uint256 amount) external override {
+        ERC20Helpers.transfer(token, msg.sender, amount);
+    }
+}
---
+++ packages/users/rainbow/fee-collector/scripts/2023121100-environment-deploy/input.bsc.ts
@@ -0,0 +1,66 @@
+import {
+  dependency,
+  DEPLOYER,
+  EnvironmentDeployment,
+  MIMIC_V2_BOT,
+  PROTOCOL_ADMIN,
+  USERS_ADMIN,
+} from '@mimic-fi/v3-deployments-lib'
+import { chainlink, fp } from '@mimic-fi/v3-helpers'
+
+/* eslint-disable no-secrets/no-secrets */
+
+//Config - Gas
+const TEN_TX_GAS = fp(0.015) //10 tx
+const QUOTA = TEN_TX_GAS.mul(10) //100 tx
+
+//Config - Fee
+const FEE_PCT = fp(0.009) // 0.9%
+
+const deployment: EnvironmentDeployment = {
+  deployer: dependency('core/deployer/v1.0.0'),
+  namespace: 'rainbow-fee-collector',
+  authorizer: {
+    from: DEPLOYER,
+    name: 'authorizer',
+    version: dependency('core/authorizer/v1.1.0'),
+    owners: [USERS_ADMIN.safe],
+  },
+  priceOracle: {
+    from: DEPLOYER,
+    name: 'price-oracle',
+    version: dependency('core/price-oracle/v1.0.0'),
+    authorizer: dependency('authorizer'),
+    signer: MIMIC_V2_BOT.address,
+    pivot: chainlink.denominations.USD,
+    feeds: [],
+  },
+  smartVault: {
+    from: DEPLOYER,
+    name: 'smart-vault',
+    version: dependency('core/smart-vault/v1.0.0'),
+    authorizer: dependency('authorizer'),
+    priceOracle: dependency('price-oracle'),
+  },
+  tasks: [],
+  permissions: {
+    from: USERS_ADMIN,
+    authorizer: dependency('authorizer'),
+    changes: [],
+  },
+  feeSettings: {
+    from: PROTOCOL_ADMIN,
+    smartVault: dependency('smart-vault'),
+    feeController: dependency('core/fee-controller/v1.0.0'),
+    maxFeePct: fp(0.02), // 2%
+    feePct: FEE_PCT,
+  },
+  relayerSettings: {
+    from: PROTOCOL_ADMIN,
+    smartVault: dependency('smart-vault'),
+    relayer: dependency('core/relayer/v1.1.0'),
+    quota: QUOTA,
+  },
+}
+
+export default deployment
