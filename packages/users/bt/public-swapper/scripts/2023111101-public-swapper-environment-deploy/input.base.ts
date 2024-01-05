44f70f9e0c558f7d481717a9b7a4ea04a320ed75
dmf7z <fernandez.daniel@gmail.com>
2024-01-04 21:56:08 -0300

    bt: deploy fee collector and public swapper for tw-android app on base

--- packages/users/bt/public-swapper/scripts/2023111101-public-swapper-environment-deploy/input.base.ts
+++ packages/users/bt/public-swapper/scripts/2023111101-public-swapper-environment-deploy/input.base.ts
@@ -1,5 +1,5 @@
 import { OP } from '@mimic-fi/v3-authorizer'
-import {  dependency, DEPLOYER, EnvironmentUpdate, USERS_ADMIN, PROTOCOL_ADMIN } from '@mimic-fi/v3-deployments-lib'
+import { dependency, DEPLOYER, EnvironmentUpdate, USERS_ADMIN, PROTOCOL_ADMIN } from '@mimic-fi/v3-deployments-lib'
 import { fp, ONES_ADDRESS } from '@mimic-fi/v3-helpers'
 
 /* eslint-disable no-secrets/no-secrets */
@@ -12,101 +12,103 @@ const BT_FEE_PCT = fp(0.007) // 0.7%
 const QUOTA = 0
 
 /*
-twandroid
-twios
-twbrowserextension
-defiandroid
-defiios
+tw-android
+tw-ios
+tw-browser-extension
+defi-android
+defi-ios
 */
 
 const update: EnvironmentUpdate = {
   deployer: dependency('core/deployer/v1.0.0'),
   namespace: 'bt-swapper',
   steps: [
-  {
-    from: DEPLOYER,
-    name: 'smart-vault-twandroid',
-    version: dependency('core/smart-vault/v1.0.0'),
-    authorizer: dependency('2023111100-public-fee-collector-environment-deploy', 'authorizer'),
-    priceOracle: dependency('2023111100-public-fee-collector-environment-deploy', 'price-oracle'),
-  },
-  //1inch public swapper
-  {
-    from: DEPLOYER,
-    name: '1inch-v5-public-swapper-twandroid',
-    version: 'OneInchV5PublicSwapper',
-    args: [dependency('core/connectors/1inch-v5/v1.0.0')],
-    config: {
-      baseConfig: {
-        smartVault: dependency('smart-vault-twandroid'),
-      },
-      tokenIndexConfig: {
-        acceptanceType: 0, //Deny list
-        tokens: [],
+    {
+      from: DEPLOYER,
+      name: 'smart-vault-tw-android',
+      version: dependency('core/smart-vault/v1.0.0'),
+      authorizer: dependency('2023111100-public-fee-collector-environment-deploy', 'authorizer'),
+      priceOracle: dependency('2023111100-public-fee-collector-environment-deploy', 'price-oracle'),
+    },
+    //1inch public swapper
+    {
+      from: DEPLOYER,
+      name: '1inch-v5-public-swapper-tw-android',
+      version: 'OneInchV5PublicSwapper',
+      initialize: 'initializePublicSwapper',
+      args: [dependency('core/connectors/1inch-v5/v1.0.0')],
+      config: {
+        baseConfig: {
+          smartVault: dependency('smart-vault-tw-android'),
+        },
+        tokenIndexConfig: {
+          acceptanceType: 0, //Deny list
+          tokens: [],
+        },
       },
     },
-  },
-  {
-    from: USERS_ADMIN,
-    authorizer: dependency('2023111100-public-fee-collector-environment-deploy', 'authorizer'),
-    changes: [
-      {
-        where: dependency('smart-vault-twandroid'),
-        revokes: [],
-        grants: [
-          {
-            who: dependency('1inch-v5-public-swapper'),
-            what: 'collect',
-            params: [],
-          },
-          {
-            who: dependency('1inch-v5-public-swapper'),
-            what: 'execute',
-            params: [
-              {
-                op: OP.EQ,
-                value: dependency('core/connectors/1inch-v5/v1.0.0'),
-              },
-            ],
-          },
-          {
-            who: dependency('1inch-v5-public-swapper'),
-            what: 'wrap',
-            params: [],
-          },
-          {
-            who: dependency('1inch-v5-public-swapper'),
-            what: 'unwrap',
-            params: [],
-          },
-          {
-            who: dependency('1inch-v5-public-swapper'),
-            what: 'withdraw',
-            params: [],
-          },
-        ],
-      },
-      {
-        where: dependency('1inch-v5-public-swapper'),
-        revokes: [],
-        grants: [{ who: ANYONE, what: 'call', params: [] }],
-      },
-    ],
-  },
-  {
-    from: PROTOCOL_ADMIN,
-    smartVault: dependency('smart-vault-twandroid'),
-    feeController: dependency('core/fee-controller/v1.0.0'),
-    feeCollector: dependency('2023111100-public-fee-collector-environment-deploy', 'depositor'), //Collector
-    maxFeePct: fp(0.02), // 2%
-    feePct: BT_FEE_PCT,
-  },
-   {
-    from: PROTOCOL_ADMIN,
-    smartVault: dependency('smart-vault-twandroid'),
-    relayer: dependency('core/relayer/v1.1.0'),
-    quota: QUOTA,
-  },
+    {
+      from: USERS_ADMIN,
+      authorizer: dependency('2023111100-public-fee-collector-environment-deploy', 'authorizer'),
+      changes: [
+        {
+          where: dependency('smart-vault-tw-android'),
+          revokes: [],
+          grants: [
+            {
+              who: dependency('1inch-v5-public-swapper-tw-android'),
+              what: 'collect',
+              params: [],
+            },
+            {
+              who: dependency('1inch-v5-public-swapper-tw-android'),
+              what: 'execute',
+              params: [
+                {
+                  op: OP.EQ,
+                  value: dependency('core/connectors/1inch-v5/v1.0.0'),
+                },
+              ],
+            },
+            {
+              who: dependency('1inch-v5-public-swapper-tw-android'),
+              what: 'wrap',
+              params: [],
+            },
+            {
+              who: dependency('1inch-v5-public-swapper-tw-android'),
+              what: 'unwrap',
+              params: [],
+            },
+            {
+              who: dependency('1inch-v5-public-swapper-tw-android'),
+              what: 'withdraw',
+              params: [],
+            },
+          ],
+        },
+        {
+          where: dependency('1inch-v5-public-swapper-tw-android'),
+          revokes: [],
+          grants: [{ who: ANYONE, what: 'call', params: [] }],
+        },
+      ],
+    },
+    {
+      from: PROTOCOL_ADMIN,
+      smartVault: dependency('smart-vault-tw-android'),
+      feeController: dependency('core/fee-controller/v1.0.0'),
+      feeCollector: dependency('2023111100-public-fee-collector-environment-deploy', 'depositor'), //Collector
+      maxFeePct: fp(0.02), // 2%
+      feePct: BT_FEE_PCT,
+    },
+    {
+      from: PROTOCOL_ADMIN,
+      smartVault: dependency('smart-vault-tw-android'),
+      relayer: dependency('core/relayer/v1.1.0'),
+      quota: QUOTA,
+    },
+  ],
 }
 
 export default update
