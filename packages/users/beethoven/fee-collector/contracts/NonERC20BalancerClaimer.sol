d68b18b65a73e5db7b0db99c161c467ff7cfa1bb
lgalende <lau@mimic.fi>
2024-01-08 22:30:11 -0300

    beethoven: implement custom collector task for non-erc20 tokens

---
+++ packages/users/beethoven/fee-collector/contracts/NonERC20BalancerClaimer.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+pragma solidity ^0.8.0;
+
+import '@mimic-fi/v3-helpers/contracts/utils/ERC20Helpers.sol';
+
+import './BalancerClaimer.sol';
+import './interfaces/INonERC20BalancerClaimer.sol';
+
+/**
+ * @title Non-ERC20 Balancer claimer
+ * @dev Task used to claim non-ERC20 tokens from Balancer's protocol fee withdrawer
+ */
+contract NonERC20BalancerClaimer is INonERC20BalancerClaimer, BalancerClaimer {
+    // Balance of the smart vault before executing the Balancer claimer task
+    uint256 private balanceBefore;
+
+    /**
+     * @dev Disables the default balancer claimer initializer
+     */
+    function initializeBalancerClaimer(TaskConfig memory, address, address) external pure override {
+        revert TaskInitializerDisabled();
+    }
+
+    /**
+     * @dev Initializes the non-ERC20 balancer claimer
+     * @param config Task config
+     * @param withdrawer Protocol fee withdrawer address
+     * @param collector Protocol fee collector address
+     */
+    function initializeNonERC20BalancerClaimer(TaskConfig memory config, address withdrawer, address collector)
+        external
+        virtual
+        initializer
+    {
+        __NonERC20BalancerClaimer_init(config, withdrawer, collector);
+    }
+
+    /**
+     * @dev Initializes the non-ERC20 balancer claimer. It does call upper contracts initializers.
+     * @param config Task config
+     * @param withdrawer Protocol fee withdrawer address
+     * @param collector Protocol fee collector address
+     */
+    function __NonERC20BalancerClaimer_init(TaskConfig memory config, address withdrawer, address collector)
+        internal
+        onlyInitializing
+    {
+        __BalancerClaimer_init(config, withdrawer, collector);
+        __NonERC20BalancerClaimer_init_unchained(config, withdrawer, collector);
+    }
+
+    /**
+     * @dev Initializes the non-ERC20 balancer claimer. It does not call upper contracts initializers.
+     * @param config Task config
+     * @param withdrawer Protocol fee withdrawer address
+     * @param collector Protocol fee collector address
+     */
+    function __NonERC20BalancerClaimer_init_unchained(TaskConfig memory config, address withdrawer, address collector)
+        internal
+        onlyInitializing
+    {
+        // solhint-disable-previous-line no-empty-blocks
+    }
+
+    /**
+     * @dev Before balancer claimer task hook
+     */
+    function _beforeBalancerClaimer(address token, uint256 amount) internal override {
+        super._beforeBalancerClaimer(token, amount);
+        balanceBefore = IERC20(token).balanceOf(smartVault);
+    }
+
+    /**
+     * @dev After balancer claimer task hook
+     */
+    function _afterBalancerClaimer(address token, uint256) internal override {
+        uint256 balanceAfter = IERC20(token).balanceOf(smartVault);
+        uint256 amount = balanceAfter - balanceBefore;
+        super._afterBalancerClaimer(token, amount);
+    }
+}
---
+++ packages/users/beethoven/fee-collector/test/NonERC20BalancerClaimer.test.ts
@@ -0,0 +1,343 @@
+import {
+  assertEvent,
+  assertIndirectEvent,
+  assertNoEvent,
+  BigNumberish,
+  bn,
+  deploy,
+  deployProxy,
+  fp,
+  getSigners,
+  ZERO_ADDRESS,
+  ZERO_BYTES32,
+} from '@mimic-fi/v3-helpers'
+import { buildEmptyTaskConfig, deployEnvironment } from '@mimic-fi/v3-tasks'
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address'
+import { expect } from 'chai'
+import { Contract, ethers } from 'ethers'
+
+describe('NonERC20BalancerClaimer', () => {
+  let task: Contract, smartVault: Contract, authorizer: Contract, protocolFeeWithdrawer: Contract
+  let owner: SignerWithAddress, other: SignerWithAddress, protocolFeesCollector: SignerWithAddress
+
+  before('setup', async () => {
+    // eslint-disable-next-line prettier/prettier
+    [, owner, other, protocolFeesCollector] = await getSigners()
+    ;({ authorizer, smartVault } = await deployEnvironment(owner))
+  })
+
+  beforeEach('deploy task', async () => {
+    protocolFeeWithdrawer = await deploy('ProtocolFeeWithdrawerMock', [protocolFeesCollector.address])
+    task = await deployProxy(
+      'NonERC20BalancerClaimer',
+      [],
+      [buildEmptyTaskConfig(owner, smartVault), protocolFeeWithdrawer.address, protocolFeesCollector.address],
+      'initializeNonERC20BalancerClaimer'
+    )
+  })
+
+  describe('initialization', async () => {
+    it('cannot call parent initialize', async () => {
+      await expect(
+        task.initializeBalancerClaimer(
+          buildEmptyTaskConfig(owner, smartVault),
+          protocolFeeWithdrawer.address,
+          protocolFeesCollector.address
+        )
+      ).to.be.revertedWith('TaskInitializerDisabled')
+    })
+
+    it('has a withdrawer reference', async () => {
+      expect(await task.protocolFeeWithdrawer()).to.be.equal(protocolFeeWithdrawer.address)
+    })
+
+    it('has a collector reference', async () => {
+      expect(await task.protocolFeesCollector()).to.be.equal(protocolFeesCollector.address)
+    })
+  })
+
+  describe('BalancerClaimer', () => {
+    describe('execution type', () => {
+      it('defines it correctly', async () => {
+        const expectedType = ethers.utils.solidityKeccak256(['string'], ['BALANCER_CLAIMER'])
+        expect(await task.EXECUTION_TYPE()).to.be.equal(expectedType)
+      })
+    })
+
+    describe('setProtocolFeeWithdrawer', () => {
+      context('when the sender is authorized', () => {
+        beforeEach('set sender', async () => {
+          const setProtocolFeeWithdrawerRole = task.interface.getSighash('setProtocolFeeWithdrawer')
+          await authorizer.connect(owner).authorize(owner.address, task.address, setProtocolFeeWithdrawerRole, [])
+          task = task.connect(owner)
+        })
+
+        context('when the given address is not zero', () => {
+          it('sets the protocol fee withdrawer', async () => {
+            await task.setProtocolFeeWithdrawer(other.address)
+
+            expect(await task.protocolFeeWithdrawer()).to.be.equal(other.address)
+          })
+
+          it('emits an event', async () => {
+            const tx = await task.setProtocolFeeWithdrawer(other.address)
+
+            await assertEvent(tx, 'ProtocolFeeWithdrawerSet', { protocolFeeWithdrawer: other })
+          })
+        })
+
+        context('when the given address is zero', () => {
+          it('reverts', async () => {
+            await expect(task.setProtocolFeeWithdrawer(ZERO_ADDRESS)).to.be.revertedWith(
+              'TaskProtocolFeeWithdrawerZero'
+            )
+          })
+        })
+      })
+
+      context('when the sender is not authorized', () => {
+        beforeEach('set sender', () => {
+          task = task.connect(other)
+        })
+
+        it('reverts', async () => {
+          await expect(task.setProtocolFeeWithdrawer(other.address)).to.be.revertedWith('AuthSenderNotAllowed')
+        })
+      })
+    })
+
+    describe('setProtocolFeesCollector', () => {
+      context('when the sender is authorized', () => {
+        beforeEach('set sender', async () => {
+          const setProtocolFeesCollectorRole = task.interface.getSighash('setProtocolFeesCollector')
+          await authorizer.connect(owner).authorize(owner.address, task.address, setProtocolFeesCollectorRole, [])
+          task = task.connect(owner)
+        })
+
+        context('when the given address is not zero', () => {
+          it('sets the protocol fees collector', async () => {
+            await task.setProtocolFeesCollector(other.address)
+
+            expect(await task.protocolFeesCollector()).to.be.equal(other.address)
+            expect(await task.getTokensSource()).to.be.equal(other.address)
+          })
+
+          it('emits an event', async () => {
+            const tx = await task.setProtocolFeesCollector(other.address)
+
+            await assertEvent(tx, 'ProtocolFeesCollectorSet', { protocolFeesCollector: other })
+          })
+        })
+
+        context('when the given address is zero', () => {
+          it('reverts', async () => {
+            await expect(task.setProtocolFeesCollector(ZERO_ADDRESS)).to.be.revertedWith(
+              'TaskProtocolFeesCollectorZero'
+            )
+          })
+        })
+      })
+
+      context('when the sender is not authorized', () => {
+        beforeEach('set sender', () => {
+          task = task.connect(other)
+        })
+
+        it('reverts', async () => {
+          await expect(task.setProtocolFeesCollector(other.address)).to.be.revertedWith('AuthSenderNotAllowed')
+        })
+      })
+    })
+
+    describe('setBalanceConnectors', () => {
+      context('when the sender is authorized', () => {
+        beforeEach('authorize sender', async () => {
+          const setBalanceConnectorsRole = task.interface.getSighash('setBalanceConnectors')
+          await authorizer.connect(owner).authorize(owner.address, task.address, setBalanceConnectorsRole, [])
+          task = task.connect(owner)
+        })
+
+        const itCanBeSet = (previous: string, next: string) => {
+          it('can be set', async () => {
+            const tx = await task.setBalanceConnectors(previous, next)
+
+            const connectors = await task.getBalanceConnectors()
+            expect(connectors.previous).to.be.equal(previous)
+            expect(connectors.next).to.be.equal(next)
+
+            await assertEvent(tx, 'BalanceConnectorsSet', { previous, next })
+          })
+        }
+
+        context('when setting to non-zero', () => {
+          const next = '0x0000000000000000000000000000000000000000000000000000000000000001'
+
+          context('when setting previous to zero', () => {
+            const previous = ZERO_BYTES32
+
+            itCanBeSet(previous, next)
+          })
+
+          context('when setting previous to non-zero', () => {
+            const previous = '0x0000000000000000000000000000000000000000000000000000000000000002'
+
+            it('reverts', async () => {
+              await expect(task.setBalanceConnectors(previous, next)).to.be.revertedWith('TaskPreviousConnectorNotZero')
+            })
+          })
+        })
+
+        context('when setting to zero', () => {
+          const previous = ZERO_BYTES32
+          const next = ZERO_BYTES32
+
+          itCanBeSet(previous, next)
+        })
+      })
+
+      context('when the sender is not authorized', () => {
+        it('reverts', async () => {
+          await expect(task.setBalanceConnectors(ZERO_BYTES32, ZERO_BYTES32)).to.be.revertedWith('AuthSenderNotAllowed')
+        })
+      })
+    })
+
+    describe('call', () => {
+      beforeEach('authorize task', async () => {
+        const callRole = smartVault.interface.getSighash('call')
+        await authorizer.connect(owner).authorize(task.address, smartVault.address, callRole, [])
+      })
+
+      context('when the sender is authorized', () => {
+        beforeEach('authorize sender', async () => {
+          const callRole = task.interface.getSighash('call')
+          await authorizer.connect(owner).authorize(owner.address, task.address, callRole, [])
+        })
+
+        context('when the token to claim is not the address zero', () => {
+          let token: Contract
+
+          const fee = fp(0.02) // 2%
+
+          beforeEach('deploy token', async () => {
+            token = await deploy('TokenWithTransferFeeMock', ['USDC', fee])
+          })
+
+          context('when the amount is greater than zero', () => {
+            const totalBalance = fp(100)
+
+            beforeEach('fund protocol fees collector', async () => {
+              await token.mint(protocolFeesCollector.address, totalBalance)
+              await token.connect(protocolFeesCollector).approve(protocolFeeWithdrawer.address, totalBalance)
+            })
+
+            const itExecutesTheTaskProperly = (requestedAmount: BigNumberish) => {
+              const transactedAmount = bn(requestedAmount).eq(0) ? totalBalance : requestedAmount
+
+              it('calls the call primitive', async () => {
+                const tx = await task.connect(owner).call(token.address, requestedAmount)
+
+                const data = protocolFeeWithdrawer.interface.encodeFunctionData('withdrawCollectedFees', [
+                  [token.address],
+                  [transactedAmount],
+                  smartVault.address,
+                ])
+
+                await assertIndirectEvent(tx, smartVault.interface, 'Called', {
+                  target: protocolFeeWithdrawer,
+                  data,
+                  value: 0,
+                })
+              })
+
+              it('transfers the token in from the protocol fee withdrawer to the smart vault', async () => {
+                const previousSmartVaultBalance = await token.balanceOf(smartVault.address)
+                const previousFeesCollectorBalance = await token.balanceOf(protocolFeesCollector.address)
+
+                await task.connect(owner).call(token.address, requestedAmount)
+
+                const amountAfterFee = bn(transactedAmount).mul(fp(1).sub(fee)).div(fp(1))
+                const currentSmartVaultBalance = await token.balanceOf(smartVault.address)
+                expect(currentSmartVaultBalance).to.be.eq(previousSmartVaultBalance.add(amountAfterFee))
+
+                const currentFeesCollectorBalance = await token.balanceOf(protocolFeesCollector.address)
+                expect(currentFeesCollectorBalance).to.be.eq(previousFeesCollectorBalance.sub(transactedAmount))
+              })
+
+              it('emits an Executed event', async () => {
+                const tx = await task.connect(owner).call(token.address, requestedAmount)
+
+                await assertIndirectEvent(tx, task.interface, 'Executed')
+              })
+            }
+
+            context('without balance connectors', () => {
+              const requestedAmount = totalBalance.div(2)
+
+              itExecutesTheTaskProperly(requestedAmount)
+
+              it('does not update any balance connectors', async () => {
+                const tx = await task.connect(owner).call(token.address, requestedAmount)
+
+                await assertNoEvent(tx, 'BalanceConnectorUpdated')
+              })
+            })
+
+            context('with balance connectors', () => {
+              const requestedAmount = 0
+              const nextConnectorId = '0x0000000000000000000000000000000000000000000000000000000000000002'
+
+              beforeEach('set balance connectors', async () => {
+                const setBalanceConnectorsRole = task.interface.getSighash('setBalanceConnectors')
+                await authorizer.connect(owner).authorize(owner.address, task.address, setBalanceConnectorsRole, [])
+                await task.connect(owner).setBalanceConnectors(ZERO_BYTES32, nextConnectorId)
+              })
+
+              beforeEach('authorize task to update balance connectors', async () => {
+                const updateBalanceConnectorRole = smartVault.interface.getSighash('updateBalanceConnector')
+                await authorizer
+                  .connect(owner)
+                  .authorize(task.address, smartVault.address, updateBalanceConnectorRole, [])
+              })
+
+              itExecutesTheTaskProperly(requestedAmount)
+
+              it('updates the balance connectors properly', async () => {
+                const tx = await task.connect(owner).call(token.address, requestedAmount)
+
+                await assertIndirectEvent(tx, smartVault.interface, 'BalanceConnectorUpdated', {
+                  id: nextConnectorId,
+                  token,
+                  amount: totalBalance.mul(fp(1).sub(fee)).div(fp(1)),
+                  added: true,
+                })
+              })
+            })
+          })
+
+          context('when the amount is zero', () => {
+            const amount = 0
+
+            it('reverts', async () => {
+              await expect(task.connect(owner).call(token.address, amount)).to.be.revertedWith('TaskAmountZero')
+            })
+          })
+        })
+
+        context('when the token to claim is the zero address', () => {
+          const token = ZERO_ADDRESS
+
+          it('reverts', async () => {
+            await expect(task.connect(owner).call(token, 1)).to.be.revertedWith('TaskTokenZero')
+          })
+        })
+      })
+
+      context('when the sender is not authorized', () => {
+        it('reverts', async () => {
+          await expect(task.call(ZERO_ADDRESS, 0)).to.be.revertedWith('AuthSenderNotAllowed')
+        })
+      })
+    })
+  })
+})
