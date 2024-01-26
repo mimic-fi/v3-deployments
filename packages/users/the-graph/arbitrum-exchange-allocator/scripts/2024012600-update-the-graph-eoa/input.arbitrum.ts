9bcba037a5c7bef076bd4e28b6f787146f960fed
dmf7z <fernandez.daniel@gmail.com>
2024-01-26 13:49:09 -0300

    update the graph eoa

---
+++ packages/users/the-graph/arbitrum-exchange-allocator/scripts/2024012600-update-the-graph-eoa/input.arbitrum.ts
@@ -0,0 +1,56 @@
+import { dependency, DEPLOYER, EnvironmentUpdate, USERS_ADMIN } from '@mimic-fi/v3-deployments-lib'
+
+const THE_GRAPH_FUNDER = '0xeDFEd47b96834338DFd71967Ab15AFaD4E7b7c2E'
+
+const update: EnvironmentUpdate = {
+  deployer: dependency('core/deployer/v1.0.0'),
+  namespace: 'the-graph',
+  steps: [
+    {
+      from: USERS_ADMIN,
+      authorizer: dependency('2023071500-environment-deploy', 'authorizer'),
+      changes: [
+        {
+          where: dependency('2023071500-environment-deploy', 'collector-exchange-allocator'),
+          grants: [{ who: DEPLOYER.address, what: 'setTokensSource', params: [] }],
+          revokes: [],
+        },
+        {
+          where: dependency('2023071500-environment-deploy', 'collector-relayer-funder'),
+          grants: [{ who: DEPLOYER.address, what: 'setTokensSource', params: [] }],
+          revokes: [],
+        },
+      ],
+    },
+    {
+      from: DEPLOYER,
+      target: dependency('2023071500-environment-deploy', 'collector-exchange-allocator'),
+      method: 'setTokensSource',
+      args: [THE_GRAPH_FUNDER],
+    },
+    {
+      from: DEPLOYER,
+      target: dependency('2023071500-environment-deploy', 'collector-relayer-funder'),
+      method: 'setTokensSource',
+      args: [THE_GRAPH_FUNDER],
+    },
+    {
+      from: USERS_ADMIN,
+      authorizer: dependency('2023071500-environment-deploy', 'authorizer'),
+      changes: [
+        {
+          where: dependency('2023071500-environment-deploy', 'collector-exchange-allocator'),
+          revokes: [{ who: DEPLOYER.address, what: 'setTokensSource' }],
+          grants: [],
+        },
+        {
+          where: dependency('2023071500-environment-deploy', 'collector-relayer-funder'),
+          revokes: [{ who: DEPLOYER.address, what: 'setTokensSource' }],
+          grants: [],
+        },
+      ],
+    },
+  ],
+}
+
+export default update
