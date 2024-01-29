12aa70c8839bcb77c498bfbd0ef97a9bb9aca6fa
dmf7z <fernandez.daniel@gmail.com>
2024-01-29 15:29:02 -0300

    exodus: use wormhole bridge in polygon

---
+++ packages/users/exodus/fee-collector/scripts/2024012900-add-wormhole-to-polygon-non-1inch/input.polygon.ts
@@ -0,0 +1,111 @@
+import { balanceConnectorId, dependency, DEPLOYER, EnvironmentUpdate, USERS_ADMIN } from '@mimic-fi/v3-deployments-lib'
+import { bn, DAY, fp, tokens } from '@mimic-fi/v3-helpers'
+
+/* eslint-disable no-secrets/no-secrets */
+
+const TIMELOCK_MODE = {
+  SECONDS: 0,
+  ON_DAY: 1,
+  ON_LAST_DAY: 2,
+  EVERY_X_MONTH: 3,
+}
+
+//Config - Tokens
+const USDC = tokens.polygon.USDC
+
+//Config - Addresses
+const MAINNET_DEPOSITOR_TASK = '0xFC5194Ec5797009180218b6E31dbeAb4df1D7eb5'
+
+//Config - Threshold
+const USDC_CONVERT_THRESHOLD = bn(20e6) // 20 USDC
+
+//Config - Withdrawer Timelock
+const BRIDGER_TIMELOCK_MODE = TIMELOCK_MODE.ON_LAST_DAY //SECONDS
+const BRIDGER_TIMELOCK_FREQUENCY = 1 //1 month
+const BRIDGER_TIMELOCK_ALLOWED_AT = 1704024000 //12:00hs UTC
+const BRIDGER_TIMELOCK_WINDOW = 2 * DAY //2 days
+
+const update: EnvironmentUpdate = {
+  deployer: dependency('core/deployer/v1.0.0'),
+  namespace: 'exodus-fee-collector-non-inch',
+  steps: [
+    //Bridger by time: makes sure that by the end of the period, it bridges everything
+    {
+      from: DEPLOYER,
+      name: 'wormhole-bridger',
+      version: dependency('core/tasks/bridge/wormhole/v2.0.0'),
+      config: {
+        baseBridgeConfig: {
+          connector: dependency('core/connectors/wormhole/v1.0.0'),
+          recipient: MAINNET_DEPOSITOR_TASK,
+          destinationChain: 1, // mainnet
+          maxSlippage: fp(0.02), //2%
+          maxFee: {
+            token: USDC,
+            amount: fp(0.02), //2%
+          },
+          customDestinationChains: [],
+          customMaxSlippages: [],
+          customMaxFees: [],
+          taskConfig: {
+            baseConfig: {
+              smartVault: dependency('2023122800-environment-deploy-non-1inch', 'smart-vault'),
+              previousBalanceConnectorId: balanceConnectorId('bridger-connection'),
+            },
+            tokenIndexConfig: {
+              acceptanceType: 1,
+              tokens: [USDC],
+            },
+            timeLockConfig: {
+              mode: BRIDGER_TIMELOCK_MODE,
+              frequency: BRIDGER_TIMELOCK_FREQUENCY,
+              allowedAt: BRIDGER_TIMELOCK_ALLOWED_AT,
+              window: BRIDGER_TIMELOCK_WINDOW,
+            },
+            tokenThresholdConfig: {
+              defaultThreshold: {
+                token: USDC,
+                min: USDC_CONVERT_THRESHOLD.mul(2),
+                max: 0,
+              },
+            },
+          },
+        },
+      },
+    },
+    {
+      from: USERS_ADMIN,
+      authorizer: dependency('2023122800-environment-deploy-non-1inch', 'authorizer'),
+      changes: [
+        {
+          where: dependency('2023122800-environment-deploy-non-1inch', 'smart-vault'),
+          revokes: [],
+          grants: [
+            {
+              who: dependency('wormhole-bridger'),
+              what: 'execute',
+              params: [],
+            },
+            {
+              who: dependency('wormhole-bridger'),
+              what: 'updateBalanceConnector',
+              params: [],
+            },
+          ],
+        },
+        {
+          where: dependency('wormhole-bridger'),
+          revokes: [],
+          grants: [{ who: dependency('core/relayer/v1.1.0'), what: 'call', params: [] }],
+        },
+        {
+          where: dependency('2023122800-environment-deploy-non-1inch', 'connext-bridger'),
+          revokes: [{ who: dependency('core/relayer/v1.1.0'), what: 'call' }],
+          grants: [],
+        },
+      ],
+    },
+  ],
+}
+
+export default update
