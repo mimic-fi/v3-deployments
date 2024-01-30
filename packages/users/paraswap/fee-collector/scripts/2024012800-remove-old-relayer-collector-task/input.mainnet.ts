06a935dcd3d30799f9b85867e52c28d5f234bdbb
Daniel Fernandez <fernandez.daniel@gmail.com>
2024-01-30 07:43:35 -0300

    Paraswap: Remove old relayer collector task (#86)
    Co-authored-by: Facu Spagnuolo <facundo_spagnuolo@icloud.com>

---
+++ packages/users/paraswap/fee-collector/scripts/2024012800-remove-old-relayer-collector-task/input.mainnet.ts
@@ -0,0 +1,21 @@
+import { dependency, EnvironmentUpdate, USERS_ADMIN } from '@mimic-fi/v3-deployments-lib'
+
+const update: EnvironmentUpdate = {
+  deployer: dependency('core/deployer/v1.0.0'),
+  namespace: 'paraswap-fee-collector-v3',
+  steps: [
+    {
+      from: USERS_ADMIN,
+      authorizer: dependency('2023111700-environment-deploy', 'authorizer'),
+      changes: [
+        {
+          where: dependency('2023111700-environment-deploy', 'collector-relayer-funder'),
+          revokes: [{ who: dependency('core/relayer/v1.1.0'), what: 'call' }],
+          grants: [],
+        },
+      ],
+    },
+  ],
+}
+
+export default update
