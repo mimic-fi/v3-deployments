b7e221a12a3464ab075a5593dd12b9a514567dd7
Ramiro Gonzalez <ramiro@mimic.fi>
2024-03-20 11:30:02 -0300

    chore: update token limits

--- packages/users/balancer/fee-collector/scripts/2024031900-configure-custom-swap-volume-limit/input.avalanche.ts
+++ packages/users/balancer/fee-collector/scripts/2024031900-configure-custom-swap-volume-limit/input.avalanche.ts
@@ -5,7 +5,7 @@ import { DAY, fp, tokens } from '@mimic-fi/v3-helpers'
 
 const TOKEN = '0x594fe86e60d8df04ec9c5b2f7fec09dc14175804'
 const LIMIT_TOKEN = tokens.avalanche.USDC
-const LIMIT_AMOUNT = fp(5000)
+const LIMIT_AMOUNT = '5000e6'
 const LIMIT_PERIOD = DAY
 
 const update: EnvironmentUpdate = {
@@ -31,6 +31,16 @@ const update: EnvironmentUpdate = {
           grants: [{ who: DEPLOYER.address, what: 'setCustomVolumeLimit', params: [] }],
           revokes: [],
         },
+        {
+          where: dependency('2023101700-environment-deploy', '1inch-swapper'),
+          grants: [{ who: DEPLOYER.address, what: 'setCustomVolumeLimit', params: [] }],
+          revokes: [],
+        },
+        {
+          where: dependency('2024011600-deploy-new-ps-swapper', 'paraswap-swapper-v2'),
+          grants: [{ who: DEPLOYER.address, what: 'setCustomVolumeLimit', params: [] }],
+          revokes: [],
+        },
       ],
     },
     {
@@ -52,6 +62,18 @@ const update: EnvironmentUpdate = {
       args: [TOKEN, LIMIT_TOKEN, LIMIT_AMOUNT, LIMIT_PERIOD],
     },
     {
+      from: DEPLOYER,
+      target: dependency('2023101700-environment-deploy', '1inch-swapper'),
+      method: 'setCustomVolumeLimit',
+      args: [TOKEN, LIMIT_TOKEN, LIMIT_AMOUNT, LIMIT_PERIOD],
+    },
+    {
+      from: DEPLOYER,
+      target: dependency('2024011600-deploy-new-ps-swapper', 'paraswap-swapper-v2'),
+      method: 'setCustomVolumeLimit',
+      args: [TOKEN, LIMIT_TOKEN, LIMIT_AMOUNT, LIMIT_PERIOD],
+    },
+    {
       from: USERS_ADMIN,
       authorizer: dependency('2023101700-environment-deploy', 'authorizer'),
       changes: [
@@ -69,6 +91,16 @@ const update: EnvironmentUpdate = {
           where: dependency('2023101700-environment-deploy', 'balancer-v2-linear-swapper'),
           revokes: [{ who: DEPLOYER.address, what: 'setCustomVolumeLimit' }],
           grants: [],
+        },
+        {
+          where: dependency('2023101700-environment-deploy', '1inch-swapper'),
+          grants: [],
+          revokes: [{ who: DEPLOYER.address, what: 'setCustomVolumeLimit' }],
+        },
+        {
+          where: dependency('2024011600-deploy-new-ps-swapper', 'paraswap-swapper-v2'),
+          grants: [],
+          revokes: [{ who: DEPLOYER.address, what: 'setCustomVolumeLimit' }],
         },
       ],
     },
