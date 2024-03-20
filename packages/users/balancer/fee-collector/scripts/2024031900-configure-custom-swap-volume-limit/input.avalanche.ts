f5ff712d77773c63ce3dca10b18a896d042ce43d
Ramiro Gonzalez <ramiro@mimic.fi>
2024-03-20 11:58:58 -0300

    chore: update limit for ERC20 tokens

--- packages/users/balancer/fee-collector/scripts/2024031900-configure-custom-swap-volume-limit/input.avalanche.ts
+++ packages/users/balancer/fee-collector/scripts/2024031900-configure-custom-swap-volume-limit/input.avalanche.ts
@@ -1,5 +1,5 @@
 import { dependency, DEPLOYER, EnvironmentUpdate, USERS_ADMIN } from '@mimic-fi/v3-deployments-lib'
-import { DAY, fp, tokens } from '@mimic-fi/v3-helpers'
+import { DAY, tokens } from '@mimic-fi/v3-helpers'
 
 /* eslint-disable no-secrets/no-secrets */
 
