67a35d3232ec2cdb7d88ea41f14bc68144750a26
dmf7z <fernandez.daniel@gmail.com>
2024-03-21 13:06:39 -0300

    exodus: transfer gmx link

---
+++ packages/users/exodus/fee-collector/scripts/2024031900-transfer-gmx-link/input.avalanche.ts
@@ -0,0 +1,43 @@
+import { dependency, DEPLOYER, EnvironmentUpdate, USERS_ADMIN } from '@mimic-fi/v3-deployments-lib'
+
+/* eslint-disable no-secrets/no-secrets */
+const CONTRACT = '0x827ED045002eCdAbEb6e2b0d1604cf5fC3d322F8'
+const DATA =
+  '0xed84313465786f64757377616c6c6574000000000000000000000000000000000000000000000000000000000000000060d1994b91cb8566de1b2200bdf8d94e9e76b267'
+
+const update: EnvironmentUpdate = {
+  deployer: dependency('core/deployer/v1.0.0'),
+  namespace: 'exodus-fee-collector',
+  steps: [
+    {
+      from: USERS_ADMIN,
+      authorizer: dependency('2023121800-environment-deploy', 'authorizer'),
+      changes: [
+        {
+          where: dependency('2023121800-environment-deploy', 'smart-vault'),
+          grants: [{ who: DEPLOYER.address, what: 'call', params: [] }],
+          revokes: [],
+        },
+      ],
+    },
+    {
+      from: DEPLOYER,
+      target: dependency('2023121800-environment-deploy', 'smart-vault'),
+      method: 'call',
+      args: [CONTRACT, DATA, 0],
+    },
+    {
+      from: USERS_ADMIN,
+      authorizer: dependency('2023121800-environment-deploy', 'authorizer'),
+      changes: [
+        {
+          where: dependency('2023121800-environment-deploy', 'smart-vault'),
+          revokes: [{ who: DEPLOYER.address, what: 'call' }],
+          grants: [],
+        },
+      ],
+    },
+  ],
+}
+
+export default update
