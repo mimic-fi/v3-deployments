6900830b7e3a6966c9f9d4127832feadb5671135
dmf7z <fernandez.daniel@gmail.com>
2024-03-25 16:49:32 -0300

    mimic: fix lint

--- packages/users/mimic/fee-collector/scripts/2024032500-set-collectors/input.arbitrum.ts
+++ packages/users/mimic/fee-collector/scripts/2024032500-set-collectors/input.arbitrum.ts
@@ -4,18 +4,18 @@ const update: EnvironmentUpdate = {
   deployer: dependency('core/deployer/v1.0.0'),
   namespace: 'mimic-fee-collector',
   steps: [
-    // {
-    //   from: PROTOCOL_ADMIN,
-    //   target: dependency('core/relayer/v1.1.0'),
-    //   method: 'setDefaultCollector',
-    //   args: [MIMIC_V2_BOT.address],
-    // },
-    // {
-    //   from: PROTOCOL_ADMIN,
-    //   target: dependency('core/relayer/v2.0.0'),
-    //   method: 'setDefaultCollector',
-    //   args: [MIMIC_V2_BOT.address],
-    // },
+    {
+      from: PROTOCOL_ADMIN,
+      target: dependency('core/relayer/v1.1.0'),
+      method: 'setDefaultCollector',
+      args: [MIMIC_V2_BOT.address],
+    },
+    {
+      from: PROTOCOL_ADMIN,
+      target: dependency('core/relayer/v2.0.0'),
+      method: 'setDefaultCollector',
+      args: [MIMIC_V2_BOT.address],
+    },
     {
       from: PROTOCOL_ADMIN,
       target: dependency('core/fee-controller/v1.0.0'),
