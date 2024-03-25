a789994d196fb193f8bc798abcbd0f9c7b3ae210
dmf7z <fernandez.daniel@gmail.com>
2024-03-25 16:48:35 -0300

    mimic: update collectors

---
+++ packages/users/mimic/fee-collector/scripts/2024032500-set-collectors/input.arbitrum.ts
@@ -0,0 +1,28 @@
+import { dependency, EnvironmentUpdate, PROTOCOL_ADMIN, MIMIC_V2_BOT } from '@mimic-fi/v3-deployments-lib'
+
+const update: EnvironmentUpdate = {
+  deployer: dependency('core/deployer/v1.0.0'),
+  namespace: 'mimic-fee-collector',
+  steps: [
+    // {
+    //   from: PROTOCOL_ADMIN,
+    //   target: dependency('core/relayer/v1.1.0'),
+    //   method: 'setDefaultCollector',
+    //   args: [MIMIC_V2_BOT.address],
+    // },
+    // {
+    //   from: PROTOCOL_ADMIN,
+    //   target: dependency('core/relayer/v2.0.0'),
+    //   method: 'setDefaultCollector',
+    //   args: [MIMIC_V2_BOT.address],
+    // },
+    {
+      from: PROTOCOL_ADMIN,
+      target: dependency('core/fee-controller/v1.0.0'),
+      method: 'setDefaultFeeCollector',
+      args: [dependency('2024032000-environment-deploy', 'fee-depositor')],
+    },
+  ],
+}
+
+export default update
