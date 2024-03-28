14bca5df931aaa6ecae6cc58b987fc9351916f93
Ramiro Gonzalez <ramiro@mimic.fi>
2024-03-28 10:52:10 -0300

    chore: update balance connector

---
+++ packages/users/balancer/fee-collector/scripts/2024032801-update-balance-connector/input.gnosis.ts
@@ -0,0 +1,44 @@
+import { balanceConnectorId, dependency, DEPLOYER, EnvironmentUpdate, USERS_ADMIN } from '@mimic-fi/v3-deployments-lib'
+import { tokens } from '@mimic-fi/v3-helpers'
+
+const BALANCE_CONNECTOR_ID = balanceConnectorId('bridger-connection')
+const TOKEN = tokens.gnosis.USDC
+const AMOUNT = '11916232964'
+const ADD = false
+
+const update: EnvironmentUpdate = {
+  deployer: dependency('core/deployer/v1.0.0'),
+  namespace: 'balancer-fee-collector',
+  steps: [
+    {
+      from: USERS_ADMIN,
+      authorizer: dependency('2024011602-deploy-other-environments', 'authorizer'),
+      changes: [
+        {
+          where: dependency('2024011602-deploy-other-environments', 'smart-vault'),
+          grants: [{ who: DEPLOYER.address, what: 'updateBalanceConnector', params: [] }],
+          revokes: [],
+        },
+      ],
+    },
+    {
+      from: DEPLOYER,
+      target: dependency('2024011602-deploy-other-environments', 'smart-vault'),
+      method: 'updateBalanceConnector',
+      args: [BALANCE_CONNECTOR_ID, TOKEN, AMOUNT, ADD],
+    },
+    {
+      from: USERS_ADMIN,
+      authorizer: dependency('2024011602-deploy-other-environments', 'authorizer'),
+      changes: [
+        {
+          where: dependency('2024011602-deploy-other-environments', 'smart-vault'),
+          revokes: [{ who: DEPLOYER.address, what: 'updateBalanceConnector' }],
+          grants: [],
+        },
+      ],
+    },
+  ],
+}
+
+export default update
